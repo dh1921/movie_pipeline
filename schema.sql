40f6d9cb8bfc861e56a8b70e684472acfd68aba4
Dinesh T <dineshdineshh321123@gmail.com>
2025-10-28 15:43:46 +0530

    Add files via upload

---
+++ schema.sql
@@ -0,0 +1,29 @@
+USE movie_db;
+
+CREATE TABLE IF NOT EXISTS movies (
+    movie_id INT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    year INT,
+    director VARCHAR(255) DEFAULT 'Unknown',
+    rating DECIMAL(3,1),
+    genres VARCHAR(255),
+    decade INT,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS ratings (
+    userId INT NOT NULL,
+    movieId INT NOT NULL,
+    rating DECIMAL(2,1) NOT NULL,
+    timestamp BIGINT,
+    PRIMARY KEY (userId, movieId),
+    FOREIGN KEY (movieId) REFERENCES movies(movie_id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+);
+
+
+select * from movies;
+
+
+
