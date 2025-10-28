8b07cb9ea02980d6bc65fbdfcf06dcd2237e61a8
Dinesh T <dineshdineshh321123@gmail.com>
2025-10-29 01:53:39 +0530

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -5,46 +5,57 @@ USE movie_db;
 -- Which movie has the highest average rating?
 
 SELECT title, rating
-FROM movies
-WHERE rating IS NOT NULL
-ORDER BY rating DESC
-LIMIT 1;
+FROM (
+    SELECT title, rating,
+	ROW_NUMBER() OVER (ORDER BY rating DESC) AS row_num
+    FROM movies
+    WHERE rating IS NOT NULL
+) ranked
+WHERE row_num = 1;
 
 
 -- What are the top 5 movie genres that have the highest average rating?
 
-SELECT TRIM(SUBSTRING_INDEX(SUBSTRING_INDEX(genres, ',', n.n), ',', -1)) AS genre,
-ROUND(AVG(rating), 2) AS avg_rating
-FROM movies
-JOIN (
-        SELECT a.N + b.N * 10 + 1 AS n
-        FROM 
-            (SELECT 0 AS N UNION SELECT 1 UNION SELECT 2 UNION SELECT 3 UNION SELECT 4
-             UNION SELECT 5 UNION SELECT 6 UNION SELECT 7 UNION SELECT 8 UNION SELECT 9) a,
-            (SELECT 0 AS N UNION SELECT 1 UNION SELECT 2 UNION SELECT 3 UNION SELECT 4
-             UNION SELECT 5 UNION SELECT 6 UNION SELECT 7 UNION SELECT 8 UNION SELECT 9) b
-    ) n
-WHERE n.n <= 1 + LENGTH(genres) - LENGTH(REPLACE(genres, ',', ''))
-GROUP BY genre
-ORDER BY avg_rating DESC
-LIMIT 5;
+WITH genre_avg AS (
+    SELECT TRIM(SUBSTRING_INDEX(SUBSTRING_INDEX(genres, ',', n.n), ',', -1)) AS genre,
+	ROUND(AVG(rating), 2) AS avg_rating
+    FROM movies
+    JOIN (
+        SELECT 1 AS n UNION ALL SELECT 2 UNION ALL SELECT 3 UNION ALL SELECT 4 UNION ALL SELECT 5
+    ) n ON n.n <= 1 + LENGTH(genres) - LENGTH(REPLACE(genres, ',', ''))
+    WHERE genres IS NOT NULL AND genres <> ''
+    GROUP BY genre
+)
+SELECT genre, avg_rating
+FROM (
+    SELECT genre, avg_rating,
+        DENSE_RANK() OVER (ORDER BY avg_rating DESC) AS rank_num
+    FROM genre_avg
+) ranked
+WHERE rank_num <= 5;
+
+
 
 
 -- Who is the director with the most movies in this dataset?
 
-SELECT director, COUNT(*) AS movie_count
-FROM movies
-WHERE director IS NOT NULL AND director <> 'Unknown'
-GROUP BY director
-ORDER BY movie_count DESC
-LIMIT 1;
+SELECT director, movie_count
+FROM (
+    SELECT director,COUNT(*) AS movie_count,
+	ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS row_num
+    FROM movies
+    WHERE director IS NOT NULL AND director <> 'Unknown'
+    GROUP BY director
+) ranked
+WHERE row_num = 1;
+
 
 
 -- What is the average rating of movies released each year?
 
-SELECT year,ROUND(AVG(rating), 2) AS avg_rating
+SELECT DISTINCT year,
+ROUND(AVG(rating) OVER (PARTITION BY year), 2) AS avg_rating
 FROM movies
 WHERE year IS NOT NULL
-GROUP BY year
 ORDER BY year;
 
